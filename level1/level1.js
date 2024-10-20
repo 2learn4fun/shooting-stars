b7792afa8d8c08177596191860d107e153d11dd1
gabrielruizmb <gabrielruizmb@gmail.com>
2024-10-20 12:00:56 -0300

    Segunda parte do desenvolvimento do nível 1

--- level1/level1.js
+++ level1/level1.js
@@ -44,7 +44,7 @@ function beginGameplay() {
 
 function newAsteroidRain() {
   spawnAsteroids();
-  setInterval(dropAsteroids, 200);
+  setInterval(dropAsteroids, 100);
 }
 
 function spawnAsteroids() {
@@ -52,7 +52,7 @@ function spawnAsteroids() {
   asteroid1Rotation = Math.random() * 360;
   asteroid1VerticalPosition = 40;
   asteroid1HorizontalPosition = 48;
-  asteroid1VerticalCadence = Math.random() * 4;
+  asteroid1Direction = Math.round(Math.random() * 3);
   asteroid1.style.rotate = `${asteroid1Rotation}deg`;
   asteroid1.style.width = `${asteroid1Width}vw`;
   asteroid1.style.top = `${asteroid1VerticalPosition}vh`;
@@ -62,12 +62,32 @@ function spawnAsteroids() {
 function dropAsteroids() {
   if(asteroidWaves < 5) {
     if(asteroid1Width < 50) {
-      asteroid1.style.rotate = `${asteroid1Rotation += 0.50}deg`;
+      asteroid1.style.rotate = `${asteroid1Rotation += 5}deg`;
       asteroid1.style.width = `${asteroid1Width += 1}vw`;
-      asteroid1.style.top = `${
-        asteroid1VerticalPosition +=asteroid1VerticalCadence
-      }vh`;
-      asteroid1.style.left = `${asteroid1HorizontalPosition +=2}vw`;
+      if(asteroid1Direction == 0) {
+        asteroid1.style.top = `${
+          asteroid1VerticalPosition +=asteroid1Direction
+        }vh`;
+        asteroid1.style.left = `${asteroid1HorizontalPosition +=2}vw`;
+      }
+      if(asteroid1Direction == 1) {
+        asteroid1.style.top = `${
+          asteroid1VerticalPosition +=asteroid1Direction
+        }vh`;
+        asteroid1.style.left = `${asteroid1HorizontalPosition -=2}vw`;
+      }
+      if(asteroid1Direction == 2) {
+        asteroid1.style.top = `${
+          asteroid1VerticalPosition -=asteroid1Direction
+        }vh`;
+        asteroid1.style.left = `${asteroid1HorizontalPosition +=2}vw`;
+      }
+      if(asteroid1Direction == 3) {
+        asteroid1.style.top = `${
+          asteroid1VerticalPosition -=asteroid1Direction
+        }vh`;
+        asteroid1.style.left = `${asteroid1HorizontalPosition -=2}vw`;
+      }
 
       if(asteroid1Width == 50) {
         spawnAsteroids();
